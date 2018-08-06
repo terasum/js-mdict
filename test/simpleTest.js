945cd4c1fde5b289375bd2f0691182c1910dd4c9
terasum <terasum@163.com>
2018-08-06 18:49:00 +0800

    feat(*): add bktree and passed the lookup test

--- test/simpleTest.js
+++ test/simpleTest.js
@@ -2,7 +2,23 @@ import Mdict from "../src/index";
 
 // const mdict = new Mdict2("mdx/ETDict.mdx");
 const mdict = new Mdict("mdx/oale8.mdx");
-for (let i = 0; i < 100; i++) {
-  mdict.lookup("youse");
-  mdict.lookup("yous");
-}
+console.log(mdict.lookup("hello"));
+
+console.log(mdict.lookup("world"));
+
+console.log(mdict.lookup("yes"));
+
+// console.log(mdict.prefix("hello"));
+// console.log(mdict.lookup("//uk/hello__gb_1.mp3"));
+// console.log(mdict.similar("hello"));
+
+// console.log(mdict.key_data[mdict.trie.lookup("2p")]);
+// const d1 = new Date().getTime();
+// for (let i = 0; i < 10; i++) {
+//   console.log(mdict.lookup("hello"));
+//   console.log(mdict.lookup("world"));
+// }
+
+// const d2 = new Date().getTime();
+// console.log("time", d2 - d1);
+
