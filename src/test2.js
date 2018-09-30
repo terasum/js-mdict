40281e02a5f57e92504833a2c64ca5f9fe6fdf12
terasum <terasum@163.com>
2018-09-30 10:34:46 +0800

    test(*): add normal test and basic benchmark

--- src/test2.js
+++
@@ -1,5 +0,0 @@
-import Mdict from "./index";
-
-// const mdict = new Mdict2("mdx/ETDict.mdx");
-const mdict = new Mdict("mdx/oale8.mdx");
-console.log(mdict);
--- test/bktreeSpec.js
+++ test/bktreeSpec.js
@@ -1,10 +1,19 @@
+import chai from "chai";
 import BKTree from "../src/bktree";
 
-// --------------------
-// BKTree tests
-// --------------------
-const words = ["浙大", "浙江大学", "浙江大学软件学院", "浙大软院", "felt", "oops", "pop", "oouch", "halt"];
+const expect = chai.expect;
 
-const bktree = new BKTree(words.length);
-bktree.add(words);
-console.log(bktree.simWords("浙大", 3));
+describe("BKtree", () => {
+  describe("#simWords", () => {
+    it("should return 3 simwords", () => {
+      const words = ["浙大", "浙江大学", "浙江大学软件学院", "浙大软院", "felt", "oops", "pop", "oouch", "halt"];
+      const bktree = new BKTree(words.length);
+      bktree.add(words);
+      const simWords = bktree.simWords("浙大", 3);
+      expect(simWords).to.be.a("array");
+      expect(simWords).to.have.lengthOf(3);
+      expect(simWords[0]).to.eq("浙大");
+    });
+  });
+});
+
