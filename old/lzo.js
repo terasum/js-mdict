b562de7ef149b6ba896c3b0201597fc747a71076
terasum <terasum@163.com>
2018-09-30 11:02:55 +0800

    chore(*): delete useless files

--- old/lzo.js
+++
@@ -1,7 +0,0 @@
-const lzo1x = require("./lzo1x.js");
-
-exports.decompress = (buf) => {
-  const state = { inputBuffer: new Uint8Array(buf) };
-  const ret = lzo1x.decompress(state);
-  return state.outputBuffer;
-};
