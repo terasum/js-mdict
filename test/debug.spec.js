8f535669ed52bfec77cb4c1eaae387109edfbfce
terasum <terasum@163.com>
2019-02-19 00:36:19 +0800

    feat(README.md): update readme.md

--- test/debug.spec.js
+++ test/debug.spec.js
@@ -5,17 +5,50 @@ import Mdict from "../src/Mdict";
 function recorder(dictName, dictPath, func) {
   const startTime = new Date().getTime();
   const mdict = new Mdict(dictPath);
+  console.log(mdict.lookup("hello"));
+  /*
+  { keyText: "hello",
+    definition: "你好",
+  }
+  */
+  console.log(mdict.prefix("hello"));
+  /*
+  [ { v: 64744840, k: 'he' },
+  { v: 65513175, k: 'hell' },
+  { v: 65552694, k: 'hello' } ]
+  */
 
-  const word = "hitch";
-  mdict.suggest(word).then((data) => {
+  let word = "informations";
+  mdict.suggest(word).then((sw) => {
     // eslint-disable-next-line
-    console.log(data);
+    console.log(sw);
+    // [ 'INFORMATION\'S', 'information' ]
   });
 
+  word = "hitch";
   const fws = mdict.fuzzy_search(word, 20, 5);
+  console.log(fws);
+  /*
+ [ { key: 'history', rofset: 66627131, ed: 4 },
+  { key: 'hit', rofset: 66648124, ed: 2 },
+  { key: 'hit back', rofset: 66697464, ed: 4 },
+  { key: 'hit back', rofset: 66697464, ed: 4 },
+  { key: 'hit big', rofset: 66698789, ed: 4 },
+  { key: 'hitch', rofset: 66698812, ed: 0 },
+  { key: 'hitched', rofset: 66706586, ed: 2 },
+  { key: 'hitcher', rofset: 66706602, ed: 2 },
+  { key: 'hitches', rofset: 66706623, ed: 2 },
+  { key: 'hitchhike', rofset: 66706639, ed: 4 },
+  { key: 'hitchhiker', rofset: 66710697, ed: 5 },
+  { key: 'hitching', rofset: 66712273, ed: 3 },
+  { key: 'hi-tech', rofset: 66712289, ed: 2 },
+  { key: 'hit for', rofset: 66713795, ed: 4 } ]
 
-  console.log(fws[0]);
+  */
   console.log(mdict.parse_defination(fws[0].key, fws[0].rofset));
+  /*
+  {}
+  */
 
   const endTime = new Date().getTime();
   const elapsedTime = endTime - startTime;
