badf0da6b29294e3b320342cc62e1453c3a1501c
danjame <danjamexie@gmail.com>
2021-04-26 11:44:46 +0800

    improved associate method for case that matched keys exist in more than one key block

--- src/Mdict.js
+++ src/Mdict.js
@@ -103,9 +103,16 @@ class Mdict extends MdictBase {
    */
   associate(phrase) {
     const sfunc = this._stripKey();
-    const kbid = this._reduceWordKeyBlock(phrase, sfunc);
+    let kbid = this._reduceWordKeyBlock(phrase, sfunc);
     const list = this._decodeKeyBlockByKBID(kbid);
-    return list.filter(item => item.keyText.startsWith(phrase))
+    const matched = list.filter(item => item.keyText.startsWith(sfunc(phrase)));
+    // in case there are matched items in the next key block
+    while (matched[matched.length-1] === list[list.length-1]) {
+      kbid ++;
+      list = this._decodeKeyBlockByKBID(kbid);
+      matched.concat(list.filter(item => item.keyText.startsWith(sfunc(phrase)))); 
+    };
+    return matched;
   }
 
   /**
