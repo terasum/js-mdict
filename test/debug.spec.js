0852fe63400869f17f0899377a3839597b86bc7d
terasum <terasum@163.com>
2021-06-22 13:26:37 +0800

    test(debug.spec.js): fixup some tests

--- test/debug.spec.js
+++ test/debug.spec.js
@@ -1,31 +1,31 @@
-import { expect, assert } from "chai";
+import { expect, assert } from 'chai';
 
-import Mdict from "../src/mdict";
+import Mdict from '../src/mdict';
 
 function recorder(dictName, dictPath, func) {
   const startTime = new Date().getTime();
   const mdict = new Mdict(dictPath);
-  console.log(mdict.lookup("hello"));
+  console.log(mdict.lookup('hello'));
   /*
   { keyText: "hello",
     definition: "你好",
   }
   */
-  console.log(mdict.prefix("hello"));
+  console.log(mdict.prefix('hello'));
   /*
   [ { v: 64744840, k: 'he' },
   { v: 65513175, k: 'hell' },
   { v: 65552694, k: 'hello' } ]
   */
 
-  let word = "informations";
+  let word = 'informations';
   mdict.suggest(word).then((sw) => {
     // eslint-disable-next-line
     console.log(sw);
     // [ 'INFORMATION\'S', 'information' ]
   });
 
-  word = "hitch";
+  word = 'hitch';
   const fws = mdict.fuzzy_search(word, 20, 5);
   console.log(fws);
   /*
@@ -54,107 +54,173 @@ function recorder(dictName, dictPath, func) {
   const elapsedTime = endTime - startTime;
   func(mdict, elapsedTime);
   // eslint-disable-next-line
-  console.log(`loading ${dictName} dict used: ${(elapsedTime) / 1000.0} s`);
+  console.log(`loading ${dictName} dict used: ${elapsedTime / 1000.0} s`);
 }
-
 
 function debugSearch(dictName, dictPath, word, callback) {
   const startTime = new Date().getTime();
   const mdict = new Mdict(dictPath);
   const wordIndex = mdict.lookup(word);
   const endTime = new Date().getTime();
-  console.log(`${dictName}: time costs ${endTime - startTime} ms, wordIndex ${wordIndex}`);
+  console.log(
+    `${dictName}: time costs ${endTime - startTime} ms, wordIndex ${wordIndex}`
+  );
   callback(wordIndex);
-  
+}
+
+function debugSearchCaseSensitive(dictName, dictPath, word, callback) {
+  const startTime = new Date().getTime();
+  const mdict = new Mdict(dictPath, { keyCaseSensitive: true, stripKey: true });
+  const wordIndex = mdict.lookup(word);
+  const endTime = new Date().getTime();
+  console.log(
+    `${dictName}: time costs ${endTime - startTime} ms, wordIndex ${wordIndex}`
+  );
+  callback(wordIndex);
 }
 
 function debugLoadResource(dictName, dictPath, word, callback) {
   const startTime = new Date().getTime();
   const mdict = new Mdict(dictPath);
-  const wordIndex = mdict.lookup(word, {resourceKey: true});
+  const wordIndex = mdict.lookup(word, { resourceKey: true });
   const endTime = new Date().getTime();
-  console.log(`${dictName}: time costs ${endTime - startTime} ms, wordIndex ${wordIndex}`);
+  console.log(
+    `${dictName}: time costs ${endTime - startTime} ms, wordIndex ${wordIndex}`
+  );
   callback(wordIndex);
-  
 }
-
 
 function debugStripKey(dictName, dictPath, word, callback) {
   const startTime = new Date().getTime();
-  const mdict = new Mdict(dictPath,{keyCaseSensitive: true, stripKey: true});
+  const mdict = new Mdict(dictPath, { keyCaseSensitive: true, stripKey: true });
   const stripfn = mdict._stripKey();
   const strippedKey = stripfn(word);
   const endTime = new Date().getTime();
-  console.log(`${dictName}: time costs ${endTime - startTime} ms, strippedKey: ${strippedKey}`);
+  console.log(
+    `${dictName}: time costs ${
+      endTime - startTime
+    } ms, strippedKey: ${strippedKey}`
+  );
   callback(strippedKey);
 }
 
 function debugNonStripKey(dictName, dictPath, word, callback) {
   const startTime = new Date().getTime();
-  const mdict = new Mdict(dictPath,{keyCaseSensitive: true, stripKey: false, resourceKey: true});
+  const mdict = new Mdict(dictPath, {
+    keyCaseSensitive: true,
+    stripKey: false,
+    resourceKey: true,
+  });
   const stripfn = mdict._stripKey();
   const strippedKey = stripfn(word);
   const endTime = new Date().getTime();
-  console.log(`${dictName}: time costs ${endTime - startTime} ms, strippedKey: ${strippedKey}`);
+  console.log(
+    `${dictName}: time costs ${
+      endTime - startTime
+    } ms, strippedKey: ${strippedKey}`
+  );
   callback(strippedKey);
 }
 
+!(function () {
+  // debug case 1:
+  recorder('01-oale8', 'mdx/testdict/oale8.mdx', (mdict) => {
+    expect(mdict._version).to.be.equal(2);
+  });
 
-!function(){
-  // // debug case 1:
-  // recorder("oale8", "mdx/oale8.mdx", (mdict) => {
-  //   expect(mdict._version).to.be.equal(2);
-  // });
+  // debug case 2
+  // https://github.com/terasum/js-mdict/pull/27
+  debugSearch(
+    '02-葡汉汉葡',
+    'mdx/testdict/dict-01-袖珍葡汉汉葡词典(简体版).mdx',
+    'Holanda',
+    (def) => {
+      assert.isTrue(
+        def.definition ===
+          '<br><img src=file://11.gif>\r\n<p><font color=blue size=4>Holanda</font>\r\n<br> 荷兰(欧洲)\r\n\u0000'
+      );
+    }
+  );
 
-  // // debug case 2
-  // // https://github.com/terasum/js-mdict/pull/27
-  // debugSearch('dict-01-phhp','mdx/dict-01-phhp.mdx', 'Holanda', (def) =>{
-  //   assert.isTrue(def.definition === '<br><img src=file://11.gif>\r\n<p><font color=blue size=4>Holanda</font>\r\n<br> 荷兰(欧洲)\r\n\u0000');
-  // });
+  // debug case 3
+  // https://github.com/terasum/js-mdict/pull/27
+  debugSearch(
+    '03-葡汉汉葡',
+    'mdx/testdict/dict-01-袖珍葡汉汉葡词典(简体版).mdx',
+    'holanda',
+    (def) => {
+      assert.isTrue(
+        def.definition ===
+          '<br><img src=file://11.gif>\r\n<p><font color=blue size=4>holanda</font>\r\n<br><font color=red> s.f. </font>\r\n<br> 洁白亚麻细布;荷兰麻布\r\n\u0000'
+      );
+    }
+  );
 
-  // // debug case 3
-  // // https://github.com/terasum/js-mdict/pull/27
-  // debugSearch('dict-01-phhp','mdx/dict-01-phhp.mdx', 'holanda', (def) =>{
-  //   assert.isTrue(def.definition === '<br><img src=file://11.gif>\r\n<p><font color=blue size=4>holanda</font>\r\n<br><font color=red> s.f. </font>\r\n<br> 洁白亚麻细布;荷兰麻布\r\n\u0000') }
-  // )
+  // debug case 4
+  debugStripKey(
+    '04-葡汉汉葡',
+    'mdx/testdict/dict-01-袖珍葡汉汉葡词典(简体版).mdx',
+    'Holanda',
+    (strippedKey) => {
+      assert.isTrue(strippedKey === 'Holanda');
+    }
+  );
+  // debug case 5
+  debugStripKey(
+    '05-葡汉汉葡',
+    'mdx/testdict/dict-01-袖珍葡汉汉葡词典(简体版).mdx',
+    'holanda',
+    (strippedKey) => {
+      assert.isTrue(strippedKey === 'holanda');
+    }
+  );
+  // debug case 6
+  debugStripKey(
+    '06-大小写敏感',
+    'mdx/testdict/dict-03-ptDict_KeyCaseSensitive.mdx',
+    'Holanda',
+    (strippedKey) => {
+      assert.isTrue(strippedKey === 'Holanda');
+    }
+  );
 
-  // // debug case 4
-  // debugStripKey('dict-01-phhp','mdx/dict-01-phhp.mdx', 'Holanda', (strippedKey) =>{
-  //   assert.isTrue(strippedKey === 'Holanda');
-  // })
-  // // debug case 5
-  // debugStripKey('dict-01-phhp','mdx/dict-01-phhp.mdx', 'holanda', (strippedKey) =>{
-  //   assert.isTrue(strippedKey === 'holanda');
-  // })
-  // // debug case 6
-  // debugStripKey('dict-03-keySensitive', 'mdx/dict-03-ptDict_KeyCaseSensitive.mdx', 'Holanda', (strippedKey) =>{
-  //   assert.isTrue(strippedKey === 'Holanda');
-  // })
+  // debug case 7
+  debugSearchCaseSensitive(
+    '07-大小写敏感',
+    'mdx/testdict/dict-03-ptDict_KeyCaseSensitive.mdx',
+    'Holanda',
+    (def) => {
+      console.log(def);
+      assert.isTrue(def.keyText === 'Holanda');
+    }
+  );
+  // debug case 8
+  debugSearch(
+    '08-葡汉汉葡',
+    'mdx/testdict/dict-01-袖珍葡汉汉葡词典(简体版).mdx',
+    'Holanda',
+    (def) => {
+      console.log(def);
+      assert.isTrue(def.keyText === 'Holanda');
+    }
+  );
 
-  //   // debug case 7
-  // debugSearch('dict-03-keySensitive', 'mdx/dict-03-ptDict_KeyCaseSensitive.mdx', 'Holanda', (def) =>{
-  //     console.log(def);
-  //     assert.isTrue(def.keyText === 'Holanda');
-  // })
-  // // debug case 8
-  // debugSearch('dict-01-phhp', 'mdx/dict-01-phhp.mdx', 'Holanda', (def) =>{
-  //     console.log(def);
-  //     assert.isTrue(def.keyText === 'Holanda');
-  // })
+  debugNonStripKey(
+    '09-oale8',
+    'mdx/testdict/oale8.mdx',
+    '/uk/headache__gb_1.mp3',
+    (def) => {
+      console.log(def);
+    }
+  );
 
-  // debugNonStripKey('oale8', 'mdx/oale8.mdx', '/uk/headache__gb_1.mp3', (def) =>{
-  //   console.log(def);
-  // })
-
-  debugLoadResource('oale8', 'mdx/oale8.mdd', "\\us_pron.png", (def) => {
-    console.log(def.keyText);
-    console.log(def.definition);
-  })
-
-  // debugRangeOver('oale8', 'mdx/oale8.mdd', '\\uk\\headache__gb_1.mp3', (def) =>{
-  //   console.log(def);
-  // })
-
-}();
-
-
+  debugLoadResource(
+    '10-oale8',
+    'mdx/testdict/oale8.mdd',
+    '\\us_pron.png',
+    (def) => {
+      console.log(def.keyText);
+      console.log(def.definition);
+    }
+  );
+})();
--- test/load.spec.js
+++ test/load.spec.js
@@ -1,6 +1,6 @@
-import { expect } from "chai";
+import { expect } from 'chai';
 
-import Mdict from "../src/mdict-base";
+import Mdict from '../src/mdict-base';
 
 function recorder(dictName, dictPath, func) {
   const startTime = new Date().getTime();
@@ -8,17 +8,16 @@ function recorder(dictName, dictPath, func) {
   const endTime = new Date().getTime();
   const elapsedTime = endTime - startTime;
   func(mdict, elapsedTime);
-  console.log(`loading ${dictName} dict used: ${(elapsedTime) / 1000.0} s`);
+  console.log(`loading ${dictName} dict used: ${elapsedTime / 1000.0} s`);
 }
 
 const totalTime = [];
 
 for (let i = 0; i < 10; i++) {
-  recorder("oale8", "mdx/oale8.mdx", (mdict, elapsedTime) => {
+  recorder('oale8', 'mdx/testdict/oale8.mdx', (mdict, elapsedTime) => {
     totalTime.push(elapsedTime);
     expect(mdict._version).to.be.equal(2);
   });
 }
 const avgTime = totalTime.reduce((prev, cur) => prev + cur) / totalTime.length;
 console.log(`average load time:${avgTime / 1000.0} s`);
-
