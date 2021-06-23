9cdc7e5dc77db90e3c7c7d009f7c667269dbefc5
Chen Quan <terasum@163.com>
2021-06-23 20:55:07 +0800

    fix(test): fix tests and example code (#44)

--- example/example01.js
+++
@@ -1,9 +0,0 @@
-
-import Mdict from "../src";
-
-// loading dictionary
-const mdict = new Mdict("mdx/oale8.mdx");
-console.log(mdict.lookup("interactive"));
-console.log(mdict.bsearch("interactive"));
-console.log(mdict.fuzzy_search("interactive", 5));
-console.log(mdict.prefix("interactive"));
