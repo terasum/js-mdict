f44d84f9f4c6d58ebd13168d6f3bed25c5affebd
Xiaocheng Song <songxcheng@outlook.com>
2021-06-23 14:33:08 +0800

    fix(mdict.js): correct property name (#33)

--- src/mdict.js
+++ src/mdict.js
@@ -69,9 +69,9 @@ class Mdict extends MdictBase {
       i + 1 >= list.length
         ? this._recordBlockStartOffset +
           this.recordBlockInfoList[this.recordBlockInfoList.length - 1]
-            .keyBlockDecompAccumulator +
+            .decompAccumulator +
           this.recordBlockInfoList[this.recordBlockInfoList.length - 1]
-            .keyBlockDecompSize
+            .decompSize
         : list[i + 1].recordStartOffset;
     const data = this._decodeRecordBlockByRBID(
       rid,
@@ -261,9 +261,9 @@ class Mdict extends MdictBase {
       idx + 1 >= list.length
         ? this._recordBlockStartOffset +
           this.recordBlockInfoList[this.recordBlockInfoList.length - 1]
-            .keyBlockDecompAccumulator +
+            .decompAccumulator +
           this.recordBlockInfoList[this.recordBlockInfoList.length - 1]
-            .keyBlockDecompSize
+            .decompSize
         : list[idx + 1].recordStartOffset;
     const data = this._decodeRecordBlockByRBID(
       rid,
