1141a90e1cadc0f9471106c5b50a2a571b6e5a03
terasum <terasum@163.com>
2021-06-23 18:29:04 +0800

    fix(mdict.js): refix binary search not found case

--- src/mdict.js
+++ src/mdict.js
@@ -63,7 +63,7 @@ class Mdict extends MdictBase {
     const list = this._decodeKeyBlockByKBID(kbid);
     const i = this._binarySearh(list, word, sfunc);
     // if not found the key block, return undefined
-    if (i === -1) return { keyText: word, definition: null };
+    if (!list[i]) return { keyText: word, definition: null };
     const rid = this._reduceRecordBlock(list[i].recordStartOffset);
     const nextStart =
       i + 1 >= list.length
