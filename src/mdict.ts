b2393ea129f401fa060a04f0c65fc96679e6f53d
Quan Chen <terasum@163.com>
2026-03-20 01:08:01 +0800

    Feat/relicense agpl (#89)
    * chore: relicense from MIT to AGPL-3.0
    
    * chore: bump version to v7.0.0 and add licensing transition notice

--- src/mdict.ts
+++ src/mdict.ts
@@ -1,3 +1,20 @@
+/*
+ * js-mdict - *.mdx/*.mdd interpreter
+ * Copyright (C) 2026 terasum <terasum@163.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import MdictBase, { KeyWordItem, KeyInfoItem, MDictOptions } from './mdict-base.js';
 import common from './utils.js';
 import lzo1x from './lzo1x-wrapper.js';
